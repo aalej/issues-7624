f2fa4cddfa7585069b23f8e549d5f720aba6da23
Alejandro Marco Alejandre <alejandromarco@google.com>
2024-09-06 20:05:14 +0800

    first commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,10 @@
+// Not really used, just a sanity check to see if a test passes
+it("should add 1 + 1", async () => {
+    expect(1 + 1).toEqual(2);
+});
+
+it("should get a response from hosting emulator", async () => {
+    const response = await fetch("http://127.0.0.1:5000/hello-world.txt")
+    const text = await response.text()
+    expect(text).toEqual("hello world!");
+});
